225038ccc25e80b40466823251b253fad6053c84
redfullmoon9-cmd <redfullmoon9@gmail.com>
2026-02-20 12:31:48 +0900

    first coomit

---
+++ build/CMakeFiles/CppProSFML.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/main.cpp" "CMakeFiles/CppProSFML.dir/main.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/main.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/Bullet.cpp" "CMakeFiles/CppProSFML.dir/source/Bullet.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/Bullet.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/DrawTest.cpp" "CMakeFiles/CppProSFML.dir/source/DrawTest.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/DrawTest.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/DrawTest2.cpp" "CMakeFiles/CppProSFML.dir/source/DrawTest2.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/DrawTest2.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/DrawTest3.cpp" "CMakeFiles/CppProSFML.dir/source/DrawTest3.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/DrawTest3.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/DrawTest4.cpp" "CMakeFiles/CppProSFML.dir/source/DrawTest4.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/DrawTest4.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/Enemy.cpp" "CMakeFiles/CppProSFML.dir/source/Enemy.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/Enemy.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/Game2D.cpp" "CMakeFiles/CppProSFML.dir/source/Game2D.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/Game2D.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/InputManager.cpp" "CMakeFiles/CppProSFML.dir/source/InputManager.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/InputManager.cpp.o.d"
+  "/home/hotphotatos/cNcppGameDev/CppProSFML/source/Player.cpp" "CMakeFiles/CppProSFML.dir/source/Player.cpp.o" "gcc" "CMakeFiles/CppProSFML.dir/source/Player.cpp.o.d"
+  "" "CppProSFML" "gcc" "CMakeFiles/CppProSFML.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CppProSFML.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppProSFML.dir/link.d"
+  "CMakeFiles/CppProSFML.dir/main.cpp.o"
+  "CMakeFiles/CppProSFML.dir/main.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/Bullet.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/Bullet.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest2.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest2.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest3.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest3.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest4.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/DrawTest4.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/Enemy.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/Enemy.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/Game2D.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/Game2D.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/InputManager.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/InputManager.cpp.o.d"
+  "CMakeFiles/CppProSFML.dir/source/Player.cpp.o"
+  "CMakeFiles/CppProSFML.dir/source/Player.cpp.o.d"
+  "CppProSFML"
+  "CppProSFML.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppProSFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
